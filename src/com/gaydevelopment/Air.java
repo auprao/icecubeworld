a913a5841318e6b458929aed41868789df4b713b
SuperGarryGamer <supergarrygamer@gmail.com>
2023-10-28 11:57:35 +0200

    add more blocks

---
+++ src/com/gaydevelopment/Air.java
@@ -0,0 +1,8 @@
+package com.gaydevelopment;
+
+public class Air extends GameObject{
+
+    public Air(int x, int y) {
+        super(x, y, "empty.png");
+    }
+}
--- src/com/gaydevelopment/Ground.java
+++ src/com/gaydevelopment/Ground.java
@@ -2,7 +2,7 @@ package com.gaydevelopment;
 
 public class Ground extends GameObject{
 
-    public Ground(int x, int y, String filePath) {
+    public Ground(int x, int y) {
         super(x, y, "file:trump.jpg");
         this.setScaleX(0.3);
         this.setScaleY(0.3);
