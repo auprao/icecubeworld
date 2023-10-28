b044f6871ee647727ef32d7e07f55bc1093de209
SuperGarryGamer <supergarrygamer@gmail.com>
2023-10-28 10:39:19 +0200

    empty scene

--- src/com/gaydevelopment/Main.java
+++ src/com/gaydevelopment/Main.java
@@ -1,8 +1,24 @@
 package com.gaydevelopment;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    static int width = 1280;
+    static int height = 720;
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root,1280, 720);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
